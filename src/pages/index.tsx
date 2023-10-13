b6f583bdf0bdda9da38ba697d27d29cc3251f563
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-10-13 11:22:42 +0200

    Working on header.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,34 +4,8 @@ import Head from "next/head";
 import Link from "next/link";
 import { useEffect } from "react";
 
-import { useSession, signIn, signOut } from "next-auth/react"
-function Component() {
-  const { data: session } = useSession()
 
-  async function handleAuth() {
-    if (session) {
-      await signOut()
-      return;
-    }
 
-    await signIn();
-  }
-
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn("google")}>Sign in</button>
-    </>
-  )
-}
 
 // export async function getServerSideProps(context: GetServerSidePropsContext) {
 //   const session = await getServerAuthSession({
@@ -75,9 +49,7 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center">
         <h1>Task Management Board</h1>
-        <Link href={"/api/auth"}>Login</Link>
         <div className="p-4">
-        <Component />
         </div>
       </main>
     </>
