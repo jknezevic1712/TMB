edcb4642cf67140a2123cb1ce927f2659ec911da
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-10-16 17:32:42 +0200

    DatePicker incorrect pickable dates bugfix.

--- src/components/molecules/datePicker/index.tsx
+++ src/components/molecules/datePicker/index.tsx
@@ -37,7 +37,7 @@ export default function DatePicker({ field }: { field: any }) {
           selected={field.value}
           onSelect={field.onChange}
           disabled={(date) =>
-            date > new Date() || date < new Date("1900-01-01")
+            date < new Date() || date < new Date("1900-01-01")
           }
           initialFocus
         />
