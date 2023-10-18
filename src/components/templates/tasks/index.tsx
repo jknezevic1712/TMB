daee7680d9908f5c0d1d7fdcfe4919da63452835
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-10-18 18:15:44 +0200

    Implemented icon inside of input field to reset description filter.

--- src/components/templates/tasks/index.tsx
+++ src/components/templates/tasks/index.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 // components
 import { Input } from "@/components/atoms/input";
 import AddNewDialog from "../../organisms/addNewTaskDialog";
+import { X } from "lucide-react";
 // custom hooks
 import useStore from "@/lib/hooks/useStore";
 // utils
@@ -24,12 +25,20 @@ export default function TasksTemplate() {
   return (
     <div className="flex w-full flex-col items-center justify-start gap-2">
       <div className="flex w-full items-center justify-between">
-        <Input
-          className="max-w-xs border-zinc-300"
-          placeholder="Filter by description..."
-          value={filter}
-          onChange={(e) => setFilter(e.target.value)}
-        />
+        <div className="relative">
+          <Input
+            className="max-w-xs border-zinc-300 pr-10"
+            placeholder="Filter by description..."
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+          />
+          <div
+            className="absolute right-1 top-0 mt-2 cursor-pointer"
+            onClick={() => setFilter("")}
+          >
+            <X />
+          </div>
+        </div>
         <AddNewDialog
           name="Create New"
           title="New Task"
