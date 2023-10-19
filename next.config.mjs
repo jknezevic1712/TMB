8d077a812b1c33496b19be191f9b1736f6d147f7
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-10-19 18:00:10 +0200

    Added docker files.
    Modified next config file to include standalone output so that the build can work with docker and added swcMinify property so that the js code is minified using swc.

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,8 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  swcMinify: true,
+  output: "standalone",
 };
 
 export default config;
